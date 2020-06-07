b90ff1ca4b6e197bfe2b1ccbbc7be83d4c4561ed
Zullyvan <Zullyvan@gmail.com>
2020-06-07 15:57:18 -0500

    primer commit

---
+++ app_old.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'application/json'} );
+    
+    let salida = {
+        nombre: 'zullyvan',
+        edad: 39,
+        url: req.url
+    }
+
+    res.write(JSON.stringify(salida));
+    res.end();
+})
+.listen(8080);
+
+console.log('Escuchando puerto 8080');
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public') );
+
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: "zullyvan"
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+
+app.listen(port, () =>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
